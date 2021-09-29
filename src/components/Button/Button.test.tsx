37d9803f659f6876524e02dcca6bd262c4ffc4a6
emrahsonm3z <emrah.sonmez@itsbilisim.com>
2021-09-29 12:26:13 +0300

    Configured style directory structure and stylelint settings.

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -8,3 +8,15 @@ it('runs correctly', () => {
 
   expect(wrapper).toMatchSnapshot()
 })
+
+describe('Hello, Enzyme!', () => {
+  it('renders', () => {
+    const wrapper = shallow(<Button />)
+    expect(wrapper.html()).toMatch(/Click/)
+  })
+
+  it('renders snapshots, too', () => {
+    const wrapper = shallow(<Button />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})

cfd3d278eaffa868e051435461f11cd4b149fdcc
emrahsonm3z <emrah.sonmez@itsbilisim.com>
2021-10-07 15:02:43 +0300

    Created enviroment variable folders.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { Button } from 'components/Button'
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
       <Button />
+      <hr />
+      <h4>API_URL:</h4>
+      {process.env.REACT_APP_API_URL}
     </div>
   )
 }

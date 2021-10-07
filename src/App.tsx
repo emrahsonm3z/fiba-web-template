1e97af3f35fc6310d3273be34d33fe414c62ad31
emrahsonm3z <emrah.sonmez@itsbilisim.com>
2021-10-07 15:03:11 +0300

    husky test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function App() {
   return (
     <div className="App">
-      <Button />
+      <Button a />
       <hr />
       <h4>API_URL:</h4>
       {process.env.REACT_APP_API_URL}
